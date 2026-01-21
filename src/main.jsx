4e7b01dee87bf6d69f31352b9a9aae2b48272e70
Sarthakagrawal213 <sarthakagrawal213@gmail.com>
2026-01-21 22:08:56 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import './app.scss'
+import "./app.scss"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
+
 
 
 
