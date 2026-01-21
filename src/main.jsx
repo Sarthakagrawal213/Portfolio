c3b15a32b4ecf4ca36202939e5a2b1bf9a7da363
Sarthakagrawal213 <sarthakagrawal213@gmail.com>
2026-01-21 21:08:29 +0530

    Fix GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./app.scss"
+import { HashRouter } from 'react-router-dom'
+import './app.scss'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
 )
+
+
 
